90b86328e0c228b6d9a1b948136a567cde6555ff
BK <bkam03@yahoo.com>
2017-05-27 09:37:08 -1000

    structure made, ready to begin testing

--- smallest_multiple.js
+++ smallest_multiple.js
@@ -8,7 +8,23 @@
  */
 module.exports = function( ceiling ) {
   // do work here
+  var possibleProduct = ceiling;  //ceiling might be first convergence point
+  var testingFactor = 0;  //dividing by larger factors means less operations
+  var multipleOfCeiling = 1;
 
+  while( isChosenFactor === false ) { //testing if possibleProduct passes tests
+    for( testingFactor = ceiling - 1; testingFactor > 1; testingFactor-- ) {
+      if( possibleProduct % testingFactor !== 0 ) {
+        break;
+      }
+    }
+    if( testingFactor <== 1 ) {
+      break;
+    } else {
+      multipleOfCeiling++;
+      possibleProduct *= multipleOfCeiling;
+    }
+  }
 
   return 0;
 };
