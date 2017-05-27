e93734197f597b8d8bee369db71671e802b9a085
BK <bkam03@yahoo.com>
2017-05-27 10:11:32 -1000

    both tests pass, by brute force?

--- smallest_multiple.js
+++ smallest_multiple.js
@@ -7,24 +7,28 @@
  *                            between 1 and `ceiling`
  */
 module.exports = function( ceiling ) {
-  // do work here
-  var possibleProduct = ceiling;  //ceiling might be first convergence point
-  var testingFactor = 0;  //dividing by larger factors means less operations
-  var multipleOfCeiling = 1;
 
+
+  // do work here
+  var productIncrement = ceiling * (ceiling - 1);
+  var possibleProduct = productIncrement;  //ceiling might be first convergence point
+  var isChosenFactor = false;
+
+  //for(var i = 0; i < 100; i++ ) {
   while( isChosenFactor === false ) { //testing if possibleProduct passes tests
-    for( testingFactor = ceiling - 1; testingFactor > 1; testingFactor-- ) {
+    for( var testingFactor = ceiling - 1; testingFactor > 1; testingFactor-- ) {
       if( possibleProduct % testingFactor !== 0 ) {
         break;
       }
     }
-    if( testingFactor <== 1 ) {
-      break;
+    if( testingFactor <= 1 ) {
+      isChosenFactor = true;
     } else {
-      multipleOfCeiling++;
-      possibleProduct *= multipleOfCeiling;
+      possibleProduct += productIncrement;
+      testingFactor = ceiling - 1;
     }
+
   }
 
-  return 0;
+  return possibleProduct;
 };
